ddcdb1f94aa9f074fda4728c0458ad9e48626288
triszt4n <trisztanpiller@gmail.com>
2022-12-27 20:49:04 +0100

    Gatsby chakra UI theme shadowing

---
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -0,0 +1,11 @@
+// src/@chakra-ui/gatsby-plugin/theme.js
+import { extendTheme } from '@chakra-ui/react'
+
+// TODO: Add your own theme overrides here e.g.: fonts, colors, etc.
+const theme = {
+  colors: {
+    primary: 'rebeccapurple',
+  },
+}
+
+export default extendTheme(theme)
