36b669c5e2d9408218c8293181bffb12d9e4f958
Gergo Radeczki <g.radeczki@gmail.com>
2023-01-21 22:46:37 +0100

    Adding icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,27 @@
+import { BMEIcon } from './bme-icon.component';
+import { ChevronIcon } from './chevron-icon.component';
+import { EmailIcon } from './email-icon.component';
+import { FacebookIcon } from './facebook-icon.component';
+import { GlobeIcon } from './globe-icon.component';
+import { IncreaseIcon } from './increase-icon.component';
+import { InstagramIcon } from './instagram-icon.component';
+import { LinkedInIcon } from './linkedin-icon.component';
+import { PhoneIcon } from './phone-icon.component';
+import { SchonherzIcon } from './schonherz-icon.component';
+import { VIKIcon } from './vik-icon.component';
+import { YouTubeIcon } from './youtube-icon.component';
+
+export {
+  ChevronIcon,
+  EmailIcon,
+  FacebookIcon,
+  GlobeIcon,
+  IncreaseIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  PhoneIcon,
+  YouTubeIcon,
+  BMEIcon,
+  SchonherzIcon,
+  VIKIcon
+};
