0cd7c00a557029f0a71ca90613d9c040e6964b88
Gergo Radeczki <g.radeczki@gmail.com>
2023-01-31 21:19:03 +0100

    Adding links

--- src/components/footer/home-footer.component.tsx
+++ src/components/footer/home-footer.component.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Link, Text } from '@chakra-ui/react';
 import { BMEIcon, SchonherzIcon, VIKIcon } from '../icons';
 
 function HomeFooter() {
   return (
     <Box display="flex" flexDirection="column" alignItems="center" bgColor="darkmode_regular" p={8} gap={8}>
-      <Box display="flex" gap={8} flexWrap="wrap">
-        <BMEIcon w="10.5rem" h="3.75rem" fill="light" />
-        <VIKIcon w="3.75rem" h="3.75rem" fill="light" />
-        <SchonherzIcon w="10.5rem" h="3.75rem" fill="light" />
+      <Box display="flex" gap={8} flexWrap="wrap" alignItems="center" flexDirection={{ base: 'column', md: 'row' }}>
+        <Link href="http://www.bme.hu/" target="_blank">
+          <BMEIcon w="10.5rem" h="3.75rem" fill="light" />
+        </Link>
+        <Link href="http://www.vik.bme.hu/" target="_blank">
+          <VIKIcon w="3.75rem" h="3.75rem" fill="light" />
+        </Link>
+        <Link href="https://svie.hu/" target="_blank">
+          <SchonherzIcon w="10.5rem" h="3.75rem" fill="light" />
+        </Link>
       </Box>
       <Text>1117 Budapest, Irinyi József utca 42, 13/19</Text>
     </Box>
