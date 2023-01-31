fdcf32649a1056bf3609e77ac3c46332478ea20a
Gergo Radeczki <g.radeczki@gmail.com>
2023-01-31 21:18:52 +0100

    Changing logo order to match footer

--- src/components/header/home-header.component.tsx
+++ src/components/header/home-header.component.tsx
@@ -5,9 +5,9 @@ import { BMEIcon, SchonherzIcon, VIKIcon } from '../icons';
 function HomeHeader() {
   return (
     <Box display="flex" columnGap={16} rowGap={8} justifyContent="center" alignItems="center" flexWrap="wrap" p={8}>
-      <Link href="https://svie.hu/" target="_blank">
-        <Box maxWidth={121.88} maxHeight={48.75}>
-          <SchonherzIcon width="100%" height="auto" fill="light" />
+      <Link href="http://www.bme.hu/" target="_blank">
+        <Box maxWidth={142.71} maxHeight={57.08}>
+          <BMEIcon width="100%" height="auto" fill="light" />
         </Box>
       </Link>
       <Link href="http://www.vik.bme.hu/" target="_blank">
@@ -15,9 +15,9 @@ function HomeHeader() {
           <VIKIcon width="100%" height="auto" fill="light" />
         </Box>
       </Link>
-      <Link href="http://www.bme.hu/" target="_blank">
-        <Box maxWidth={142.71} maxHeight={57.08}>
-          <BMEIcon width="100%" height="auto" fill="light" />
+      <Link href="https://svie.hu/" target="_blank">
+        <Box maxWidth={121.88} maxHeight={48.75}>
+          <SchonherzIcon width="100%" height="auto" fill="light" />
         </Box>
       </Link>
     </Box>
