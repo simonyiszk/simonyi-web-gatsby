09e14f683d3d8fc555a171967207445122bcb765
Gergo Radeczki <g.radeczki@gmail.com>
2023-02-01 23:04:54 +0100

    Reorganizes icons

---
+++ src/components/icons/bme/index.ts
@@ -0,0 +1,6 @@
+import { BMEIcon } from './bme-icon.component';
+import { SchonherzIcon } from './schonherz-icon.component';
+import { SimonyiFullDarkIcon, SimonyiFullLightIcon, SimonyiLogoIcon } from './simonyi-icon.component';
+import { VIKIcon } from './vik-icon.component';
+
+export { BMEIcon, SchonherzIcon, SimonyiFullDarkIcon, SimonyiFullLightIcon, SimonyiLogoIcon, VIKIcon };
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,17 +1,7 @@
-import { BMEIcon } from './bme-icon.component';
+import { BMEIcon, SchonherzIcon, SimonyiFullDarkIcon, SimonyiFullLightIcon, SimonyiLogoIcon, VIKIcon } from './bme';
+import { EmailIcon, FacebookIcon, GitHubIcon, GlobeIcon, InstagramIcon, LinkedInIcon, PhoneIcon, YouTubeIcon } from './social';
 import { ChevronIcon } from './chevron-icon.component';
-import { EmailIcon } from './email-icon.component';
-import { FacebookIcon } from './facebook-icon.component';
-import { GitHubIcon } from './github-icon.component';
-import { GlobeIcon } from './globe-icon.component';
 import { IncreaseIcon } from './increase-icon.component';
-import { InstagramIcon } from './instagram-icon.component';
-import { LinkedInIcon } from './linkedin-icon.component';
-import { PhoneIcon } from './phone-icon.component';
-import { SchonherzIcon } from './schonherz-icon.component';
-import { SimonyiFullDarkIcon, SimonyiFullLightIcon, SimonyiLogoIcon } from './simonyi-icon.component';
-import { VIKIcon } from './vik-icon.component';
-import { YouTubeIcon } from './youtube-icon.component';
 import { SocialIcon } from './social-icon.component';
 
 export {
