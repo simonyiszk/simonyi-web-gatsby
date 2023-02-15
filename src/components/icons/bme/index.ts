e1eea55ee9501d72f14bcaeac182cd870a6c8d0b
Gergo Radeczki <g.radeczki@gmail.com>
2023-02-15 21:27:03 +0100

    Removing unused icons

--- src/components/icons/bme/index.ts
+++ src/components/icons/bme/index.ts
@@ -1,6 +1,3 @@
-import { BMEIcon } from './bme-icon.component';
-import { SchonherzIcon } from './schonherz-icon.component';
 import { SimonyiFullDarkIcon, SimonyiFullLightIcon, SimonyiLogoIcon } from './simonyi-icon.component';
-import { VIKIcon } from './vik-icon.component';
 
-export { BMEIcon, SchonherzIcon, SimonyiFullDarkIcon, SimonyiFullLightIcon, SimonyiLogoIcon, VIKIcon };
+export { SimonyiFullDarkIcon, SimonyiFullLightIcon, SimonyiLogoIcon };
