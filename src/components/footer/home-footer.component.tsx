879a14e8882525bdca59d0b42dae73d06c75e269
Gergő Radeczki <g.radeczki@gmail.com>
2023-02-20 18:56:40 +0100

    Footer content

--- src/components/footer/home-footer.component.tsx
+++ src/components/footer/home-footer.component.tsx
@@ -5,6 +5,70 @@ import { StaticImage } from 'gatsby-plugin-image';
 function HomeFooter() {
   return (
     <Box display="flex" flexDirection="column" alignItems="center" bgColor="darkmode_regular" p={8} gap={8}>
+      <Box
+        width={{ base: 'auto', md: '100%' }}
+        maxWidth="1496px"
+        display="flex"
+        flexWrap="wrap"
+        flexDirection={{ base: 'column', md: 'row' }}
+        alignItems={{ base: 'flex-start', md: 'flex-start' }}
+        pb={8}
+        gap={16}
+        justifyContent="space-evenly"
+      >
+        <Box display="flex" flexDirection="column">
+          <Text as="h2" pb={4}>
+            Elérhetőség
+          </Text>
+          <Link color="simonyi_zold" href="mailto:info@simonyi.bme.hu" title="info@simonyi.bme.hu" target="_blank">
+            info@simonyi.bme.hu
+          </Link>
+          <Text mt={4}>
+            1117 Budapest
+            <br />
+            Irinyi József utca 42, 1320
+          </Text>
+        </Box>
+        <Box display="flex" flexDirection="column">
+          <Text as="h2" pb={4}>
+            Weboldalaink
+          </Text>
+          <Link color="simonyi_zold" href="https://konferencia.simonyi.bme.hu/" title="konferencia.simonyi.bme.hu" target="_blank">
+            Simonyi Konferencia
+          </Link>
+          <Link color="simonyi_zold" href="https://simonyi.bme.hu/tanfolyam/" title="simonyi.bme.hu/tanfolyam" target="_blank">
+            Simonyi tanfolyamok
+          </Link>
+          <Link
+            color="simonyi_zold"
+            href="https://old.simonyi.bme.hu/ntp2021/ntp2021.pdf"
+            title="Nemzeti Tehetség Program 2021"
+            target="_blank"
+          >
+            NTP-SZKOLL-21-0078
+          </Link>
+        </Box>
+        <Box display="flex" flexDirection="column">
+          <Text as="h2" pb={4}>
+            Közösségi hálónk
+          </Text>
+          <Link color="simonyi_zold" href="https://www.facebook.com/simonyiszk" title="facebook.com/simonyiszk" target="_blank">
+            Facebook oldal
+          </Link>
+          <Link color="simonyi_zold" href="https://www.instagram.com/simonyiszk/" title="instagram.com/simonyiszk" target="_blank">
+            Instagram profil
+          </Link>
+          <Link color="simonyi_zold" href="https://www.youtube.com/@SimonyiSzakkoli" title="youtube.com/@SimonyiSzakkoli" target="_blank">
+            YouTube csatorna
+          </Link>
+          <Link color="simonyi_zold" href="https://www.linkedin.com/company/simonyi/" title="linkedin.com/company/simonyi" target="_blank">
+            LinkedIn profil
+          </Link>
+          <Link color="simonyi_zold" href="https://github.com/simonyiszk" title="github.com/simonyiszk" target="_blank">
+            GitHub profil
+          </Link>
+        </Box>
+      </Box>
       <Box display="flex" gap={8} flexWrap="wrap" alignItems="center" flexDirection={{ base: 'column', md: 'row' }}>
         <Link href="http://www.bme.hu/" target="_blank">
           <StaticImage src="../../images/bme/bme.png" alt="Logo of BME" height={60} />
@@ -16,7 +80,6 @@ function HomeFooter() {
           <StaticImage src="../../images/bme/schonherz.png" alt="Logo of BME" height={60} />
         </Link>
       </Box>
-      <Text align="center">1117 Budapest, Irinyi József utca 42, 13/19</Text>
     </Box>
   );
 }
