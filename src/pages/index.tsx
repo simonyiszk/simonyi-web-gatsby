21449f7699aacff6bae88a4eb19e42894c2151ce
Gergő Radeczki <g.radeczki@gmail.com>
2023-02-25 23:09:57 +0100

    Fix bad links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { graphql, HeadFC, PageProps } from 'gatsby';
-import { Box, Button, Image, Link, Text } from '@chakra-ui/react';
-import { HomeLayout } from '../layout';
-import { HomeHeader } from '../components/header';
-import { ChevronIcon, SimonyiFullLightIcon } from '../components/icons';
-import { Profile } from '../components/profile';
-import { StudentGroup } from '../components/student-group';
-import Lightbox from 'yet-another-react-lightbox';
-import 'yet-another-react-lightbox/styles.css';
-import Captions from 'yet-another-react-lightbox/plugins/captions';
-import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
-import Slideshow from 'yet-another-react-lightbox/plugins/slideshow';
-import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
-import Video from 'yet-another-react-lightbox/plugins/video';
-import Zoom from 'yet-another-react-lightbox/plugins/zoom';
-import 'yet-another-react-lightbox/plugins/captions.css';
-import 'yet-another-react-lightbox/plugins/thumbnails.css';
-import { about, groups, images, profiles } from '../utils';
-import { SEO } from '../components/seo';
-import { GatsbyImage, StaticImage } from 'gatsby-plugin-image';
-import { AboutType, ProfileType, StudentGroupType } from '../types';
+import React from "react";
+import { graphql, HeadFC, PageProps } from "gatsby";
+import { Box, Button, Image, Link, Text } from "@chakra-ui/react";
+import { HomeLayout } from "../layout";
+import { HomeHeader } from "../components/header";
+import { ChevronIcon, SimonyiFullLightIcon } from "../components/icons";
+import { Profile } from "../components/profile";
+import { StudentGroup } from "../components/student-group";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Video from "yet-another-react-lightbox/plugins/video";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import { about, groups, images, profiles } from "../utils";
+import { SEO } from "../components/seo";
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
+import { AboutType, ProfileType, StudentGroupType } from "../types";
 
 function Greeting() {
   return (
@@ -41,7 +41,7 @@ function Greeting() {
           src="../images/hero/default.png"
           alt="Hero image"
           objectFit="cover"
-          style={{ height: '100%', width: '100%' }}
+          style={{ height: "100%", width: "100%" }}
           placeholder="none"
           loading="eager"
         />
@@ -57,25 +57,43 @@ function Greeting() {
         <HomeHeader />
         <Box display="flex" gap={8} flexDirection="column">
           <Box maxWidth={353} maxHeight={75} m={8}>
-            <SimonyiFullLightIcon width="100%" height="100%" filter="drop-shadow(0 4px 8px rgb(0, 0, 0, 0.6))" />
+            <SimonyiFullLightIcon
+              width="100%"
+              height="100%"
+              filter="drop-shadow(0 4px 8px rgb(0, 0, 0, 0.6))"
+            />
           </Box>
-          <Box display="flex" gap={8} flexWrap="wrap" flexDirection="row" justifyContent="space-evenly">
-            <Link href="https://termek.sch.bme.hu/" title="termek.sch.bme.hu" target="_blank">
+          <Box
+            display="flex"
+            gap={8}
+            flexWrap="wrap"
+            flexDirection="row"
+            justifyContent="space-evenly"
+          >
+            <Link
+              href="https://tanfolyam.simonyi.bme.hu/"
+              title="tanfolyam.simonyi.bme.hu"
+              target="_blank"
+            >
               <Button
                 borderWidth={2}
                 borderColor="simonyi_zold"
-                _hover={{ bgColor: 'simonyi_zold' }}
+                _hover={{ bgColor: "simonyi_zold" }}
                 bgColor="var(--chakra-colors-whiteAlpha-300)"
                 width={32}
               >
                 Tanfolyam
               </Button>
             </Link>
-            <Link href="https://tanfolyam.simonyi.bme.hu/" title="tanfolyam.simonyi.bme.hu" target="_blank">
+            <Link
+              href="https://termek.sch.bme.hu/"
+              title="termek.sch.bme.hu"
+              target="_blank"
+            >
               <Button
                 borderWidth={2}
                 borderColor="simonyi_zold"
-                _hover={{ bgColor: 'simonyi_zold' }}
+                _hover={{ bgColor: "simonyi_zold" }}
                 bgColor="var(--chakra-colors-whiteAlpha-300)"
                 width={32}
               >
@@ -92,7 +110,11 @@ function Greeting() {
   );
 }
 
-function ImageBrowser({ imagesData }: { imagesData: { images: typeof images } }) {
+function ImageBrowser({
+  imagesData,
+}: {
+  imagesData: { images: typeof images };
+}) {
   const [index, setIndex] = React.useState(0);
   const [isOpen, setIsOpen] = React.useState(false);
 
@@ -108,11 +130,11 @@ function ImageBrowser({ imagesData }: { imagesData: { images: typeof images } })
   return (
     <Box
       display="flex"
-      flexWrap={{ md: 'wrap' }}
+      flexWrap={{ md: "wrap" }}
       gap="10px"
-      overflowX={{ base: 'scroll', md: 'hidden' }}
+      overflowX={{ base: "scroll", md: "hidden" }}
       mx={{ base: -8, md: 0 }}
-      justifyContent={{ base: 'flex-start', md: 'center', lg: 'flex-end' }}
+      justifyContent={{ base: "flex-start", md: "center", lg: "flex-end" }}
     >
       {imagesData.images.slice(0, 9).map((image, index) => (
         <Box
@@ -122,10 +144,14 @@ function ImageBrowser({ imagesData }: { imagesData: { images: typeof images } })
           bgColor="#000000"
           flexShrink="0"
           onClick={() => openLightbox(index)}
-          _hover={{ cursor: 'pointer' }}
+          _hover={{ cursor: "pointer" }}
         >
           {image.gatsbyImageData ? (
-            <GatsbyImage image={image.gatsbyImageData} alt={image.alt} style={{ height: '100%', width: '100%' }} />
+            <GatsbyImage
+              image={image.gatsbyImageData}
+              alt={image.alt}
+              style={{ height: "100%", width: "100%" }}
+            />
           ) : (
             <Image src={image.url} alt={image.alt} width="100%" height="100%" />
           )}
@@ -135,12 +161,14 @@ function ImageBrowser({ imagesData }: { imagesData: { images: typeof images } })
         open={isOpen}
         slides={imagesData.images.map((image) => {
           return {
-            src: image.gatsbyImageOriginalUrl ? image.gatsbyImageOriginalUrl : image.url,
+            src: image.gatsbyImageOriginalUrl
+              ? image.gatsbyImageOriginalUrl
+              : image.url,
             alt: image.alt,
             title: image.title,
             description: image.description,
             width: image.width,
-            height: image.height
+            height: image.height,
           };
         })}
         index={index}
@@ -151,15 +179,26 @@ function ImageBrowser({ imagesData }: { imagesData: { images: typeof images } })
   );
 }
 
-function About({ aboutData, imagesData }: { aboutData: { about: AboutType }; imagesData: { images: typeof images } }) {
+function About({
+  aboutData,
+  imagesData,
+}: {
+  aboutData: { about: AboutType };
+  imagesData: { images: typeof images };
+}) {
   return (
     <Box display="flex" flexDirection="column">
       <Text as="h1" mb="32px">
         {aboutData.about.title}
       </Text>
-      <Box display="grid" gridTemplateColumns={{ base: '1fr', lg: '1fr 1fr' }} gap="4rem">
+      <Box
+        display="grid"
+        gridTemplateColumns={{ base: "1fr", lg: "1fr 1fr" }}
+        gap="4rem"
+      >
         <Text>
-          {aboutData.about.text} <Link href={aboutData.about.wikiUrl}>(Wikipédia)</Link>
+          {aboutData.about.text}{" "}
+          <Link href={aboutData.about.wikiUrl}>(Wikipédia)</Link>
         </Text>
         <ImageBrowser imagesData={imagesData} />
       </Box>
@@ -167,36 +206,58 @@ function About({ aboutData, imagesData }: { aboutData: { about: AboutType }; ima
   );
 }
 
-function StudentGroups({ studentGroupsData }: { studentGroupsData: { groups: Array<StudentGroupType> } }) {
+function StudentGroups({
+  studentGroupsData,
+}: {
+  studentGroupsData: { groups: Array<StudentGroupType> };
+}) {
   return (
     <Box display="flex" flexDirection="column" gap="32px">
-      <Box alignSelf={{ base: 'center', md: 'flex-start' }}>
+      <Box alignSelf={{ base: "center", md: "flex-start" }}>
         <Text as="h1">Köreink</Text>
       </Box>
       <Box
         alignSelf="center"
         display="flex"
         justifyContent="center"
-        flexDirection={{ base: 'column', md: 'row' }}
+        flexDirection={{ base: "column", md: "row" }}
         gap="32px"
         flexWrap="wrap"
         width="100%"
       >
         {studentGroupsData.groups.map((group, index) => (
-          <StudentGroup key={index} name={group.name} description={group.description} logo={group.logo} socials={group.socials} />
+          <StudentGroup
+            key={index}
+            name={group.name}
+            description={group.description}
+            logo={group.logo}
+            socials={group.socials}
+          />
         ))}
       </Box>
     </Box>
   );
 }
 
-function Presidency({ presidencyData }: { presidencyData: { profiles: Array<ProfileType> } }) {
+function Presidency({
+  presidencyData,
+}: {
+  presidencyData: { profiles: Array<ProfileType> };
+}) {
   return (
     <Box display="flex" flexDirection="column" gap="2rem">
-      <Box alignSelf={{ base: 'center', md: 'flex-start' }}>
+      <Box alignSelf={{ base: "center", md: "flex-start" }}>
         <Text as="h1">Elnökség</Text>
       </Box>
-      <Box alignSelf="center" display="flex" justifyContent="center" flexDirection="row" gap="2rem" flexWrap="wrap" width="100%">
+      <Box
+        alignSelf="center"
+        display="flex"
+        justifyContent="center"
+        flexDirection="row"
+        gap="2rem"
+        flexWrap="wrap"
+        width="100%"
+      >
         {presidencyData.profiles.map((profile, index) => (
           <Profile
             key={index}
@@ -223,9 +284,9 @@ const IndexPage = ({ data }: PageProps<Queries.homePageQuery>) => {
           })?.node.childImageSharp?.original?.src || undefined,
         gatsbyImageData: data.lightbox.edges.find((edge) => {
           return `images/${edge.node.relativePath}` === image.url;
-        })?.node.childImageSharp?.gatsbyImageData
+        })?.node.childImageSharp?.gatsbyImageData,
       };
-    })
+    }),
   };
   const studentGroupsData = { groups };
   const presidencyData = {
@@ -235,17 +296,27 @@ const IndexPage = ({ data }: PageProps<Queries.homePageQuery>) => {
         profilePicture: {
           ...profile.profilePicture,
           gatsbyImageData: data.simonyi_elnokok.edges.find((edge) => {
-            return `images/${edge.node.relativePath}` === profile.profilePicture.url;
-          })?.node.childImageSharp?.gatsbyImageData
-        }
+            return (
+              `images/${edge.node.relativePath}` === profile.profilePicture.url
+            );
+          })?.node.childImageSharp?.gatsbyImageData,
+        },
       };
-    })
+    }),
   };
 
   return (
     <HomeLayout>
       <Greeting />
-      <Box maxWidth="1496px" mx="auto" p={8} display="flex" flexDirection="column" gap="calc(80px + 2rem)" pb="calc(80px + 2rem)">
+      <Box
+        maxWidth="1496px"
+        mx="auto"
+        p={8}
+        display="flex"
+        flexDirection="column"
+        gap="calc(80px + 2rem)"
+        pb="calc(80px + 2rem)"
+      >
         <Box />
         <About aboutData={aboutData} imagesData={imagesData} />
         <StudentGroups studentGroupsData={studentGroupsData} />
@@ -257,7 +328,12 @@ const IndexPage = ({ data }: PageProps<Queries.homePageQuery>) => {
 
 export const query = graphql`
   query homePage {
-    simonyi_elnokok: allFile(filter: { sourceInstanceName: { eq: "images" }, relativeDirectory: { eq: "simonyi/elnokok" } }) {
+    simonyi_elnokok: allFile(
+      filter: {
+        sourceInstanceName: { eq: "images" }
+        relativeDirectory: { eq: "simonyi/elnokok" }
+      }
+    ) {
       edges {
         node {
           relativePath
@@ -267,7 +343,12 @@ export const query = graphql`
         }
       }
     }
-    lightbox: allFile(filter: { sourceInstanceName: { eq: "images" }, relativeDirectory: { eq: "lightbox" } }) {
+    lightbox: allFile(
+      filter: {
+        sourceInstanceName: { eq: "images" }
+        relativeDirectory: { eq: "lightbox" }
+      }
+    ) {
       edges {
         node {
           relativePath
